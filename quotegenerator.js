45e93c927fa12b62ae7d906be3d7c816deffc016
james-built <jgbuilt@gmail.com>
2018-07-27 22:29:48 +1200

    added quotes to array, fixed insertion of quote to html, quote is broken because multiple call of randomQuote function causes different random number index of quote and author

--- quotegenerator.js
+++ quotegenerator.js
@@ -1,25 +1,23 @@
 var Quotes = [
-    {quote:"I said this", author:"me"},
-    {quote:"second quote", author: "god"}
+    {quote:"It would be better for me... that multitudes of men should disagree with me rather than that I, being one, should be out of harmony with myself.", author:"Gorgias"},
+    {quote:"Anyone who holds a true opinion without understanding is like a blind man on the right road.", author:"Plato"},
+    {quote:"", author: ""}
    ]
    
    document.addEventListener('DOMContentLoaded', loadQuote);
    
    function loadQuote(){
-    getElementById("text").innerHTML(randomQuote("text"))
-    getElementById("author").innerHTML(randomQuote("author"))
+    document.getElementById("text").innerHTML = randomQuote("text")
+    document.getElementById("author").innerHTML = randomQuote("author")
    }
    
    function randomQuote(element){
     do{
     var quoteIndex = Math.floor(Math.random() * 11)
     } while (quoteIndex > Quotes.length)
-      
       if (element === "text"){
         return Quotes[quoteIndex].quote;
-      } if (element === "author"){
+    } if (element === "author"){
         return Quotes[quoteIndex].author;
-      }
+    }
    }
-
-   Tom

53081e4a76dec64fdb20c5ba2598ecb8615a3fb3
james-built <jgbuilt@gmail.com>
2018-07-30 00:58:36 +1200

    added random integer generator & fixed randomised load of quote, added quote load button

--- quotegenerator.js
+++ quotegenerator.js
@@ -1,23 +1,23 @@
 var Quotes = [
     {quote:"It would be better for me... that multitudes of men should disagree with me rather than that I, being one, should be out of harmony with myself.", author:"Gorgias"},
     {quote:"Anyone who holds a true opinion without understanding is like a blind man on the right road.", author:"Plato"},
-    {quote:"I’ve said if Ivanka weren’t my daughter, perhaps I’d be dating her", author:"Donald Trump"}
+    {quote:"I’ve said if Ivanka weren’t my daughter, perhaps I’d be dating her.", author:"Donald Trump"}
    ]
+   
    
    document.addEventListener('DOMContentLoaded', loadQuote);
    
-   function loadQuote(){
-    document.getElementById("text").innerHTML = randomQuote("text")
-    document.getElementById("author").innerHTML = randomQuote("author")
-   }
-   
-   function randomQuote(element){
-    do{
-    var quoteIndex = Math.floor(Math.random() * 11)
-    } while (quoteIndex > Quotes.length)
-      if (element === "text"){
-        return Quotes[quoteIndex].quote;
-    } if (element === "author"){
-        return Quotes[quoteIndex].author;
-    }
-   }
+function loadQuote(){
+    grabbedQuote = randomQuote();
+    document.getElementById("text").innerHTML = grabbedQuote.quote;
+    document.getElementById("author").innerHTML = grabbedQuote.author;
+}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+function randomQuote(){
+    var quoteIndex = getRandomInt(0, (Quotes.length));
+    return Quotes[quoteIndex];
+}
